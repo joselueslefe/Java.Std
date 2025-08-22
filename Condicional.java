541ad744f528b6ece6f74aa5aa84e1ac7262de39
José <joselueslefelima@gmail.com>
2025-08-22 20:11:24 -0300

    mais uma aula sobre sintaxe com 1 e duas possibilidades.

--- Condicional.java
+++ Condicional.java
@@ -1,10 +1,26 @@
 public class Condicional {
     public static void main(String[] args) {
-        boolean chuva = false;
-        if (chuva ) {
-            System.out.println("Usar guarda-chuva!");
+       int n1 = 18;
+
+       if (n1 == 189) {
+            System.out.println("Você acabou de fazer 18 anos e está aprovado!!");
+            System.out.println("Obrigado pela preferência! :)");
         } else {
-            System.out.println("Sair sem guarda-chuva!");
+           System.out.println("Você não vai poder ir! Obrigado pela preferência novamente!");
+       }
+
+
+       //Sintaxe com duas possibilidades:
+
+        int n2 = 100;
+
+        if (n2 == 99) {
+            System.out.println("boa nota!");
+        } else if (n2 == 100) {
+            System.out.println("Você atingiu a pontuação desejada. Parabéns!!!");
+        } else {
+            System.out.println("Você precisa melhorar!");
         }
+
     }
 }

d0fa046c623e86dd467e5798766f50a779e94468
José <joselueslefelima@gmail.com>
2025-09-11 02:04:21 -0300

    methods

---
+++ metodos/java/Aluno.java
@@ -0,0 +1,18 @@
+package metodos.java;
+
+public class Aluno {
+
+        void DizerOi() {
+            System.out.println("Oiii");
+        }
+
+        void Pular() {
+            System.out.println("PULANDO!!");
+        }
+
+
+            int somar(int a, int b) {
+            return a + b;
+        }
+
+}
---
+++ metodos/java/Main.java
@@ -0,0 +1,9 @@
+package metodos.java;
+
+public class Main {
+    public static void main(String[] args) {
+        Aluno e = new Aluno();
+
+        System.out.println("Resultado da soma: " + e.somar(20,500));
+    }
+}

510c899ed8bfc58f5563b653640593cbb29a3bbc
José <joselueslefelima@gmail.com>
2025-09-12 13:40:38 -0300

    POO revisando hehehehe

---
+++ POO/java/Carro.java
@@ -0,0 +1,22 @@
+package POO.java;
+
+public class Carro {
+    int ano = 2022;
+    String n = "civic";
+    String cor = "preto";
+
+    int ano2 = 2024;
+    String c2 = "Branco";
+    String n2 = "Titano";
+    void Buzinar() {
+        System.out.println("Biiiiiiiiiii !!");
+    }
+
+    void Freiar() {
+        System.out.println("FREEEEEEEEE");
+    }
+
+    void Acelerar() {
+        System.out.println("Vraaaaam");
+    }
+}
---
+++ POO/java/Main.java
@@ -0,0 +1,20 @@
+package POO.java;
+
+public class Main {
+    public static void main(String[] args) {
+        Carro Carro = new Carro();
+
+        System.out.println("Nome:" + Carro.n);
+        System.out.println("Cor:" + Carro.cor);
+        System.out.println("Ano:" + Carro.ano);
+
+
+        System.out.println("Nome:" + Carro.n2);
+        System.out.println("Cor:" + Carro.c2);
+        System.out.println("Ano:" + Carro.ano2);
+
+
+    }
+
+
+    }

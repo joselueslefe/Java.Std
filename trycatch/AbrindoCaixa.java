40d5d73b10edf05d7e9e7858c1421503c6471b1b
José <joselueslefelima@gmail.com>
2025-10-01 01:05:17 -0300

    try, catch

---
+++ trycatch/AbrindoCaixa.java
@@ -0,0 +1,35 @@
+package trycatch;
+
+import java.util.Scanner;
+
+public class AbrindoCaixa {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+
+        try {
+            System.out.println("Divisão matemática");
+            System.out.println("========================");
+
+
+            System.out.println("Digite o primeiro valor: ");
+            int num1 = n.nextInt();
+
+            System.out.println("Digite o segundo valor: ");
+            int num2 = n.nextInt();
+
+            if (num1 == 0 && num2== 0){
+                System.out.println("Erro: Divisão indeterminada (0 por 0) .");
+            } else if (num1 == 0){
+                System.out.println("Erro: O primeiro valor não pode ser zero.");
+            } else if (num2 == 0) {
+                System.out.println("Erro: O segundo valor não pode ser zero");
+            }else {
+                int r = num1/num2;
+                System.out.println("Resultado: " + r);
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("Digite valores diferentes de zero");
+        }
+
+    }
+}
